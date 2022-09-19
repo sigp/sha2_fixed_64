280bcf20deb37abffe78407e2649756225b74764
Paul Hauner <paul@paulhauner.com>
2022-09-19 11:23:13 +1000

    Add safe and ring benches

--- benches/benches.rs
+++ benches/benches.rs
@@ -3,24 +3,41 @@
 extern crate test;
 use digest::Digest;
 use sha2::Sha256;
+use sha2_fixed_64::x86::{safe, sha_ni};
 use test::Bencher;
 
 const PREIMAGE: [u8; 64] = [42; 64];
-const RUNS: usize = 100_000;
+const RUNS: usize = 1_000;
 
 #[bench]
-fn optimised(b: &mut Bencher) {
+fn sha_ni(b: &mut Bencher) {
     b.iter(|| {
+        // The benches will always fail with a SIGILL if the CPU doesn't support sha instructions.
+        if !sha_ni::cpu_is_supported() {
+            return;
+        }
+
         // Use `test::black_box` to prevent compiler optimizations from disregarding
         // Unused values
         test::black_box(for _ in 0..RUNS {
-            sha2_fixed_64::x86::sha256(&PREIMAGE);
+            sha_ni::sha256(&PREIMAGE);
         });
     });
 }
 
 #[bench]
-fn unoptimised(b: &mut Bencher) {
+fn safe(b: &mut Bencher) {
+    b.iter(|| {
+        // Use `test::black_box` to prevent compiler optimizations from disregarding
+        // Unused values
+        test::black_box(for _ in 0..RUNS {
+            safe::sha256(&PREIMAGE);
+        });
+    });
+}
+
+#[bench]
+fn sha2_crate(b: &mut Bencher) {
     b.iter(|| {
         // Use `test::black_box` to prevent compiler optimizations from disregarding
         // Unused values
@@ -28,6 +45,20 @@ fn unoptimised(b: &mut Bencher) {
             let mut digest = Sha256::new();
             digest.update(&PREIMAGE);
             let _: [u8; 32] = digest.finalize().into();
+        });
+    });
+}
+
+#[bench]
+fn ring_crate(b: &mut Bencher) {
+    b.iter(|| {
+        // Use `test::black_box` to prevent compiler optimizations from disregarding
+        // Unused values
+        test::black_box(for _ in 0..RUNS {
+            let mut context = ring::digest::Context::new(&ring::digest::SHA256);
+            context.update(&PREIMAGE);
+            let mut output = [0; 32];
+            output.copy_from_slice(context.finish().as_ref());
         });
     });
 }
