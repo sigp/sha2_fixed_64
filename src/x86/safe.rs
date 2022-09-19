41a6e7f914e24c654945d6da0e28323c4c8becd1
Paul Hauner <paul@paulhauner.com>
2022-09-19 11:24:27 +1000

    Remove unused functions

--- src/x86/safe.rs
+++ src/x86/safe.rs
@@ -247,48 +247,6 @@ macro_rules! digest_block {
     };
 }
 
-/// Process a block with the SHA-256 algorithm.
-fn sha256_digest_block_u32(state: &mut [u32; 8], block: &[u32; 16]) {
-    let mut abef = [state[0], state[1], state[4], state[5]];
-    let mut cdgh = [state[2], state[3], state[6], state[7]];
-
-    // Rounds 0..64
-    let mut w0 = [block[3], block[2], block[1], block[0]];
-    let mut w1 = [block[7], block[6], block[5], block[4]];
-    let mut w2 = [block[11], block[10], block[9], block[8]];
-    let mut w3 = [block[15], block[14], block[13], block[12]];
-    let mut w4;
-
-    rounds4!(abef, cdgh, w0, 0);
-    rounds4!(abef, cdgh, w1, 1);
-    rounds4!(abef, cdgh, w2, 2);
-    rounds4!(abef, cdgh, w3, 3);
-    schedule_rounds4!(abef, cdgh, w0, w1, w2, w3, w4, 4);
-    schedule_rounds4!(abef, cdgh, w1, w2, w3, w4, w0, 5);
-    schedule_rounds4!(abef, cdgh, w2, w3, w4, w0, w1, 6);
-    schedule_rounds4!(abef, cdgh, w3, w4, w0, w1, w2, 7);
-    schedule_rounds4!(abef, cdgh, w4, w0, w1, w2, w3, 8);
-    schedule_rounds4!(abef, cdgh, w0, w1, w2, w3, w4, 9);
-    schedule_rounds4!(abef, cdgh, w1, w2, w3, w4, w0, 10);
-    schedule_rounds4!(abef, cdgh, w2, w3, w4, w0, w1, 11);
-    schedule_rounds4!(abef, cdgh, w3, w4, w0, w1, w2, 12);
-    schedule_rounds4!(abef, cdgh, w4, w0, w1, w2, w3, 13);
-    schedule_rounds4!(abef, cdgh, w0, w1, w2, w3, w4, 14);
-    schedule_rounds4!(abef, cdgh, w1, w2, w3, w4, w0, 15);
-
-    let [a, b, e, f] = abef;
-    let [c, d, g, h] = cdgh;
-
-    state[0] = state[0].wrapping_add(a);
-    state[1] = state[1].wrapping_add(b);
-    state[2] = state[2].wrapping_add(c);
-    state[3] = state[3].wrapping_add(d);
-    state[4] = state[4].wrapping_add(e);
-    state[5] = state[5].wrapping_add(f);
-    state[6] = state[6].wrapping_add(g);
-    state[7] = state[7].wrapping_add(h);
-}
-
 pub fn sha256(bytes: &[u8]) -> [u8; 32] {
     let mut state = crate::consts::H256_256;
 
@@ -305,18 +263,4 @@ pub fn sha256(bytes: &[u8]) -> [u8; 32] {
     output[24..28].copy_from_slice(&state[6].to_be_bytes());
     output[28..32].copy_from_slice(&state[7].to_be_bytes());
     output
-}
-
-pub fn compress(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
-    let mut block_u32 = [0u32; BLOCK_LEN];
-    // since LLVM can't properly use aliasing yet it will make
-    // unnecessary state stores without this copy
-    let mut state_cpy = *state;
-    for block in blocks {
-        for (o, chunk) in block_u32.iter_mut().zip(block.chunks_exact(4)) {
-            *o = u32::from_be_bytes(chunk.try_into().unwrap());
-        }
-        sha256_digest_block_u32(&mut state_cpy, &block_u32);
-    }
-    *state = state_cpy;
 }
